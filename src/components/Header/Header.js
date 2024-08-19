a0c1c484dab9714a510e0f773f98f03caa2599b1
Mohamed Amr <mohamedamr2014.ma@gmail.com>
2024-08-19 16:00:20 +0300

    menu-animation

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,6 +3,7 @@ import "./HeaderStyles.css"
 import navy_logo from "../../assets/navy_logo.png"
 import MenuIcon from '@mui/icons-material/Menu';
 import CancelIcon from '@mui/icons-material/Cancel';
+import { AnimatePresence,motion } from 'framer-motion';
 const Header = (props) => {
 
     
@@ -25,16 +26,17 @@ const Header = (props) => {
         </nav>
 
     </header>
-        {props.menuClicked && <div className='menu'>
+        {props.menuClicked && <AnimatePresence>
+        <motion.div initial={{y:"100%"}} animate={{y:0}} transition={{type:"tween"}} className='menu'>
             <ul className='menu-list'>
-                <li className='menu-item'><a className='menu-links' href="#home">Home</a></li>
-                <li className='menu-item'><a className='menu-links' href="#whyus">Why Us</a></li>
-                <li className='menu-item'><a className='menu-links' href="#services">Services</a></li>
-                <li className='menu-item'><a className='menu-links' href="#contact">Contact Us</a></li>
+                <motion.li className='menu-item' initial={{translateX:-50,opacity:0}} animate={{translateX:0,opacity:1}} transition={{duration:0.3,delay:0.2}}><a className='menu-links' href="#home">Home</a></motion.li>
+                <motion.li className='menu-item' initial={{translateX:-50,opacity:0}} animate={{translateX:0,opacity:1}} transition={{duration:0.3,delay:0.4}}><a className='menu-links' href="#whyus">Why Us</a></motion.li>
+                <motion.li className='menu-item' initial={{translateX:-50,opacity:0}} animate={{translateX:0,opacity:1}} transition={{duration:0.3,delay:0.6}}><a className='menu-links' href="#services">Services</a></motion.li>
+                <motion.li className='menu-item' initial={{translateX:-50,opacity:0}} animate={{translateX:0,opacity:1}} transition={{duration:0.3,delay:0.8}}><a className='menu-links' href="#contact">Contact Us</a></motion.li>
 
             </ul>
             <div className='cancel-icon-container' onClick={()=>props.handleMenuClicked()}><CancelIcon className='cancel-icon'></CancelIcon></div>
-        </div>}
+        </motion.div></AnimatePresence>}
 
         </>
   )
